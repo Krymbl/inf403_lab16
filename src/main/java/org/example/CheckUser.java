60c50542791af256b2f2792be368be76a4f08a29
Danil Romanov <danillyou@mail.ru>
2025-01-18 18:48:47 +0300

    Done full

---
+++ src/main/java/org/example/CheckUser.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class CheckUser {
+    static User admin = new User("chatop_chatop@mail.ru", "QWERTy12345+", Role.ADMIN);
+    static User user = new User("goptip_goptip@mail.ru", "TRYOp555+", Role.USER);
+    User[] users = {admin, user};
+
+    public static boolean checkLogin(String login) {
+        return login.length() >= 20 && login.matches("[a-zA-Z0-9@._-]+");
+    }
+
+    public static boolean checkPassword(String password) {
+        return password.length() >= 8 && password.matches("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[{}\\[\\](),.;&?!_\\-+]).{8,}$");
+    }
+
+    public static Role checkPasAndLog(String login, String password) {
+        if (login.equals(admin.login) && password.equals(admin.password)) {
+            return admin.role;
+        } else if (login.equals(user.login) && password.equals(user.password)) {
+            return user.role;
+        } else {
+            return Role.PROBLEM;
+        }
+    }
+}
